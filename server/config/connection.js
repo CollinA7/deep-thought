b8b75418c2930b8dacb2f9e624ce53c896d8a2f0
cayean <collinayala@gmail.com>
2022-12-05 21:55:20 -0600

    refactored config file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/deep-thoughts',
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/deep-thought',
   {
-    useNewUrlParser: true, 
+    useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
-
 module.exports = mongoose.connection;
